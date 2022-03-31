1e1b92b2b415f4164ac59922145e9a37fc817ece
Christina Loiacono <loiaconochristina@gmail.com>
2022-03-31 11:28:53 -0400

    passing all tests for food.js

--- food.js
+++ food.js
@@ -1,4 +1,27 @@
 // Create class below
+class Food{
+    constructor(name, daysToSpoil, fresh = true){
+        this.name = name;
+        this.daysToSpoil = daysToSpoil;
+        this.fresh = fresh;
+    }
+    // methods
+    prepare(){
+        console.log(`${this.name} is being prepared`);
+    }
+    isFresh(){
+        if (this.daysToSpoil > 0){
+            console.log(`There are ${this.daysToSpoil} days left before ${this.name} spoils.`);
+        } else {
+            console.log(`${this.name} has spoiled.`);
+        }
+    }
+    aDayPasses(){
+        this.daysToSpoil = this.daysToSpoil - 1;
+        console.log(`eeewww what an old ${this.name} has spoiled.`);
+        this.isFresh();
+    }
+}
 
 // Do not edit below this line
 module.exports = Food;
