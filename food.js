808e0e283031571508b8a45234d02658a0d9531a
Jenna Qian <jiayin1996@gmail.com>
2022-03-31 14:32:35 -0400

    Created Food Class

--- food.js
+++ food.js
@@ -1,4 +1,31 @@
 // Create class below
 
+class Food {
+    constructor(name, daysToSpoil, fresh = true){
+        this.name = name;
+        this.daysToSpoil = daysToSpoil;
+        this.fresh = fresh; 
+    }
+    prepare(){
+        console.log(`${this.name} is being prepared`)
+    }
+    isFresh(){
+        if(this.daysToSpoil > 0){
+            console.log(`There are ${this.daysToSpoil} days left before ${this.name} spoils.`)
+        } else {
+            console.log(`${this.name} has spoiled.`)
+        }
+    }
+    aDayPasses(){
+        this.daysToSpoil--;
+        this.isFresh()
+    }
+}
+
+const croissant = new Food("croissant", 0, true)
+console.log(croissant)
+croissant.prepare()
+croissant.isFresh()
+croissant.aDayPasses()
 // Do not edit below this line
 module.exports = Food;
--- tamagotchi.js
+++ tamagotchi.js
@@ -1,4 +1,10 @@
 // Create class below
 
+class Tamagotchi {
+    constructor(name, energy = 9, full = 8, mood = 6, sick = false, rehomed = false){
+        this.name
+    }
+}
+
 // Do not edit below this line
 module.exports = Tamagotchi;
