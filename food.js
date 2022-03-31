fec7da44b20a6c8c0c23e0aefa33b96e4d6b685f
“Lili <lilyhuang114classb@gmail.com>
2022-03-31 12:53:11 -0400

    Passed the test suites.

--- food.js
+++ food.js
@@ -1,4 +1,31 @@
 // Create class below
+class Food {
+    constructor(name, daysToSpoil, fresh = true) {
+        this.name = name;
+        this.daysToSpoil = daysToSpoil;
+        this.fresh = fresh;
+    }
 
+    prepare(){
+        console.log(`${this.name} is being prepared`)
+    }
+
+    isFresh(){
+        if(this.daysToSpoil > 0) {
+            console.log(`There are ${this.daysToSpoil} days left before ${this.name} spoils.`)
+        } else {
+            console.log(`${this.name} has spoiled.`)
+        }
+    }
+
+    aDayPasses(){
+        this.daysToSpoil -= 1;
+        this.isFresh();
+    }
+}
+
+
+
+let providedFood = new Food("ice cream", 5, false)
 // Do not edit below this line
 module.exports = Food;
