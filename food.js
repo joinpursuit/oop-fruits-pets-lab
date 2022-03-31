d6eded3dc2e4258f9bcc32ebee6e166fc54f4240
Xiaoming Yang <yangxiaoming295@gmail.com>
2022-03-31 17:25:04 -0400

    modified food.js and part of tamagotchi.js

--- food.js
+++ food.js
@@ -1,4 +1,28 @@
 // Create class below
+class Food {
+  constructor(name, daysToSpoil, fresh = true) {
+    this.name = name;
+    this.daysToSpoil = daysToSpoil;
+    this.fresh = fresh;
+  }
+  prepare() {
+    console.log(`${this.name} is being prepared`)
+  }
+  isFresh() {
+    if(this.daysToSpoil > 0){
+      console.log(`There are 3 days left before ${this.name} spoils.`)
+    } else {
+      console.log(`${this.name} has spoiled.`)
+    }
+  }
+  aDayPasses(){
+    this.daysToSpoil--
+    this.isFresh()
+  }
+}
 
+const banana = new Food("Banana", 3);
+console.log(banana);
+banana.prepare()
 // Do not edit below this line
 module.exports = Food;
--- tamagotchi.js
+++ tamagotchi.js
@@ -1,4 +1,38 @@
 // Create class below
+class Tamagotchi {
+  constructor(name, energy = 9, full = 8, mood = 6, sick = false, rehomed = false){
+    this.name = name;
+    this.energy = energy;
+    this.full = full;
+    this.mood = mood;
+    this.sick = sick;
+    this.rehomed = rehomed;
+  }
+  greet() {
+    console.log(`Hello, I'm ${this.name}!`)
+  }
+  status() {
+    console.log(`My mood is: ${this.mood}
+                 I am this full: ${this.full}
+                 My energy is: ${this.energy}
+                 I am not sick`)
+  }
+  eat() {
+    this.full += 2;
+    this.energy--
+    if(this.full>10){
+      this.sick = true;
+    }
+  }
+  medicate() {
+
+  }
+
+}
+
+const fizz = new Tamagotchi("Fizz");
+
+console.log(fizz)
 
 // Do not edit below this line
 module.exports = Tamagotchi;
