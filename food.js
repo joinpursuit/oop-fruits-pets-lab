ac69dd7abc95b79a2b1b7617721f49a99c51afd9
salrey <tekdehn@gmail.com>
2022-03-31 16:12:49 -0400

    Set Food methods

--- food.js
+++ food.js
@@ -6,6 +6,23 @@ class Food {
         this.daysToSpoil = daysToSpoil
         this.fresh = fresh
     }
+
+    prepare() {
+        console.log(`${this.name} is being prepared`)
+    }
+
+    isFresh() {
+        if (this.fresh && this.daysToSpoil > 0) {
+            console.log(`There are ${this.daysToSpoil} days left before ${this.name} spoils.`)
+        } else {
+            console.log(`${this.name} has spoiled.`)
+        }
+    }
+
+    aDayPasses() {
+        this.daysToSpoil--
+        this.isFresh()
+    }
 }
 
 // Do not edit below this line
