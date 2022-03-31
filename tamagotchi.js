a011443bae4c18d72f2d225d306aa5dcd5f21d3d
Sara Perkaj <saraperkaj@pursuit.org>
2022-03-31 17:32:34 -0400

    fixed my salad, all tests passing for both.

--- tamagotchi.js
+++ tamagotchi.js
@@ -1,4 +1,105 @@
 // Create class below
 
+class Tamagotchi {
+  constructor(
+    name,
+    energy = 9,
+    full = 8,
+    mood = 6,
+    sick = false,
+    rehomed = false
+  ) {
+    this.name = name;
+    this.energy = energy;
+    this.full = full;
+    this.mood = mood;
+    this.sick = sick;
+    this.rehomed = rehomed;
+  }
+
+  greet() {
+    console.log(`Hello I'm ${this.name}`);
+  }
+
+  status() {
+    console.log(
+      `My mood is: ${this.mood}\nI am this full: ${this.full}\nMy energy is: ${
+        this.energy
+      }\n${this.sick === false ? `I am not sick` : `I am sick`}`
+    );
+  }
+
+  eat() {
+    this.full += 2;
+    this.energy--;
+    if (this.full > 10) {
+      this.sick = true;
+    }
+    // this.full > 10 && (this.sick = true);
+  }
+
+  medicate() {
+    if (this.sick) {
+      this.full = 9;
+      this.energy -= 3;
+      this.sick = false;
+    } else {
+      console.log(`refusal to take medicine`);
+      this.energy--;
+    }
+  }
+
+  play() {
+    if (this.sick) {
+      this.mood--;
+      this.energy--;
+    } else if (this.mood > 9) {
+      this.energy -= 2;
+      this.full--;
+    } else if (this.energy <= 3) {
+      console.log("I am too tired to play");
+      this.energy--;
+    } else {
+      this.mood += 2;
+      this.energy--;
+      this.full--;
+    }
+  }
+
+  sleep() {
+    this.energy += 4;
+    this.full -= 3;
+  }
+
+  timePasses() {
+    if (this.sick) {
+      this.mood -= 3;
+      this.energy -= 2;
+      this.full -= 2;
+    } else {
+      this.mood -= 2;
+      this.energy--;
+      this.full--;
+    }
+  }
+
+  badGuardian() {
+    if (this.energy <= 0) {
+      this.rehomed = true;
+    }
+    if (this.mood <= 0) {
+      this.rehomed = true;
+    }
+    if (this.full <= 0) {
+      this.rehomed = true;
+    }
+    console.log(`Has been rehomed.`);
+  }
+}
+
+const Rose = new Tamagotchi("Rose");
+console.log(Rose);
+console.log(Rose.badGuardian());
+console.log(Rose);
 // Do not edit below this line
 module.exports = Tamagotchi;
