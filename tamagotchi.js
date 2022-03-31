b88dc3eb81a8d1908fc6b726723a462c402fb443
RohanPursuit <rohansterling@pursuit.org>
2022-03-31 13:43:11 -0400

    add sick property

--- tamagotchi.js
+++ tamagotchi.js
@@ -1,10 +1,11 @@
 // Create class below
 class Tamagotchi{
-    constructor(name, energy = 9, full = 8, mood = 6){
+    constructor(name, energy = 9, full = 8, mood = 6, sick = false){
         this.name = name
         this.energy = energy
         this.full = full
         this.mood = mood
+        this.sick = sick
     }
 }
 // Do not edit below this line
