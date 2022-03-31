f877031046ed0a775be7775409127cc271a8893a
stephanie-lucano <stephaniemarielucano@pursuit.org>
2022-03-31 15:49:25 -0400

    Food class declared and 3 attributes added

--- food.js
+++ food.js
@@ -1,4 +1,12 @@
 // Create class below
 
+class Food {
+    constructor(name, daysToSpoil, fresh = true) {
+        this.name = name
+        this.daysToSpoil = daysToSpoil
+        this.fresh = fresh
+    }
+}
+
 // Do not edit below this line
 module.exports = Food;
