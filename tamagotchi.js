5841db077260fc7ba83ed0fb5057b93fc94ec4cf
stephanie-lucano <stephaniemarielucano@pursuit.org>
2022-03-31 18:08:36 -0400

    Following methods called: medicate, play, sleep, timePasses and badGuardian are declared im Tamagotchi class

--- tamagotchi.js
+++ tamagotchi.js
@@ -9,6 +9,82 @@ class Tamagotchi {
         this.sick = sick;
         this.rehomed = rehomed;
     }
+
+    greet() {
+        console.log(`Hello, I'm ${this.name}!`)
+    }
+
+    status() {
+        console.log("My mood is:" + this.mood)
+        console.log("I am this full:" + this.full)
+        console.log("My energy is:" + this.energy)
+        if (this.sick) {
+            console.log("I am sick")
+        } else {
+            console.log("I am not sick")
+        }
+    }
+
+    eat() {
+        this.full += 2
+        this.energy--
+        if (this.full > 10) {
+            this.sick = true
+        }
+    }
+
+    medicate() {
+        if(this.sick) {
+            this.full = 9
+            this.energy -= 3
+            this.sick = false
+        } else {
+            console.log("Refuses to take medicine")
+            this.energy--
+        }
+    }
+
+    play() {
+        if (!this.sick && this.mood <=9 && this.energy > 3) {
+            this.mood += 2
+            this.energy--
+            this.full--
+        } else if (this.sick) {
+            this.mood--
+            this.energy--
+        } else if (this.mood > 9) {
+            this.energy -= 2
+            this.full--
+        } else if (this.energy <= 3) {
+            console.log("I am too tired to play")
+            this.energy--
+        }
+    }
+
+    sleep() {
+        this.energy += 4
+        this.full -= 3
+    }
+
+    timePasses() {
+        if (!this.sick) {
+            this.mood -= 2
+            this.full--
+            this.energy--
+
+        } else {
+            this.mood -= 3
+            this.full -= 2
+            this.energy -= 2
+        }
+    }
+
+    badGuardian() {
+        console.log("Tamagotchi has been rehomed")
+        if (this.energy <=0 || this.mood <= 0 || this.full <= 0) {
+            this.rehomed = true 
+        }  
+    }
 }
 
 // Do not edit below this line
