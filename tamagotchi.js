c07cc7254ca3061542367fe13b13a3fdbb6e65b9
RohanPursuit <rohansterling@pursuit.org>
2022-03-31 16:03:17 -0400

    all test passed

--- tamagotchi.js
+++ tamagotchi.js
@@ -18,11 +18,12 @@ class Tamagotchi{
     }
 
     eat(){
-       this.full += 2
-       this.energy-- 
-       if(this.full > 10){
-           this.sick = true
-       }
+
+        this.full += 2
+        this.energy-- 
+        if(this.full > 10){
+            this.sick = true
+        }
     }
 
     medicate(){
@@ -37,15 +38,51 @@ class Tamagotchi{
     }
 
     play(){
+        console.log(this)
         //play increases mood by 2, reduces energy & full by 1
         if(this.sick){
-            console.log("No Play")
-        } else {
+            this.mood--
+            this.energy--
+        }
+        else if(this.mood > 9){
+            this.energy -= 2
+            this.full--
+        }
+        else if(this.energy <= 3){
+            console.log("I am too tired to play")
+            this.energy--
+        }
+        else {
             this.mood += 2
             this.energy--
             this.full--
         }
     }
+
+    sleep(){
+        this.energy += 4
+        this.full -= 3
+    }
+
+    timePasses(){
+        if(this.sick){
+            this.mood -= 3
+            this.full -= 2
+            this.energy -= 2
+        } else {
+            this.mood -= 2
+            this.full--
+            this.energy--
+        }
+    }
+
+    badGuardian(){
+        if(this.energy <= 0 || this.mood <= 0 ||this.full <= 0){
+            this.rehomed = true
+        }
+    }
 }
+
+//console.log(rufhrei.nameMethod())
 // Do not edit below this line
 module.exports = Tamagotchi;
