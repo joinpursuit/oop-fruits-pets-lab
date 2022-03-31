181bc411163f35505693cc28f036bb845fd39a4e
RohanPursuit <rohansterling@pursuit.org>
2022-03-31 13:41:57 -0400

    add full property

--- tamagotchi.js
+++ tamagotchi.js
@@ -1,8 +1,9 @@
 // Create class below
 class Tamagotchi{
-    constructor(name, energy = 9){
+    constructor(name, energy = 9, full = 8){
         this.name = name
         this.energy = energy
+        this.full = full
     }
 }
 // Do not edit below this line
