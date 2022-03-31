ed84e9bf5ff85671360e72fe8e6b7893c5d0c5f9
RohanPursuit <rohansterling@pursuit.org>
2022-03-31 13:33:28 -0400

    add condition, if food is spoiled

--- food.js
+++ food.js
@@ -11,6 +11,11 @@ class Food{
     }
 
     isFresh(){
+        if(this.daysToSpoil < 0){
+            console.log("eeewww what an old banana has spoiled.")
+        } else {
+            console.log(`There are ${this.daysToSpoil} days left before Banana spoils.`)
+        }
     }
 
 }
