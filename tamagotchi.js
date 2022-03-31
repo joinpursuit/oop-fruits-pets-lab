ff5d2ecee6f151577bec5855935b30e9cc832006
salrey <tekdehn@gmail.com>
2022-03-31 17:51:03 -0400

    Set rest of methods for Tamagotchi

--- tamagotchi.js
+++ tamagotchi.js
@@ -32,6 +32,58 @@ class Tamagotchi {
             this.sick = true
         }
     }
+
+    medicate() {
+        if (this.sick) {
+            this.full = 9;
+            this.energy -= 3
+            this.sick = false
+        } else {
+            console.log("Refusal to take medicine")
+            this.energy--
+        }
+    }
+
+    play() {
+        if (!this.sick && this.mood <=9 && this.energy > 3) {
+            this.mood += 2
+            this.energy--
+            this.full--
+        } else if (this.sick) {
+            this.mood--
+            this.energy--
+        } else if (this.mood > 9) {
+            this.energy -= 2
+            this.full--
+        } else if (this.energy <= 3) {
+            console.log("I am too tired to play")
+            this.energy--
+        }
+    }
+
+    sleep() {
+        this.energy += 4
+        this.full -= 3
+    }
+
+    timePasses() {
+        if (!this.sick) {
+            this.mood -= 2
+            this.full--
+            this.energy--
+        } else {
+            this.mood -= 3
+            this.full -= 2
+            this.energy -= 2
+        }
+    }
+
+    badGuardian() {
+        console.log("Tamagotchi has been rehomed")
+        if (this.energy <=0 || this.mood <= 0 || this.full <= 0) {
+            this.rehomed = true 
+        }  
+    }
 }
 
 // Do not edit below this line
