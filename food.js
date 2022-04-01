d88c86b7ada2d345561c2f81988e41f59ce2a825
Jalamang <Jalamangjanka@gmail.com>
2022-04-01 12:22:44 -0400

    OOP Javacript: created classes with their properties and methods to show their behaviours

--- food.js
+++ food.js
@@ -1,4 +1,67 @@
 // Create class below
+class Food {
+  constructor(name, daysToSpoil, fresh = true) {
+    this.name = name;
+    this.daysToSpoil = daysToSpoil;
+    this.fresh = fresh;
+  }
+
+  prepare() {
+    console.log(`${this.name} is being prepared`);
+  }
+
+  isFresh() {
+    if (this.daysToSpoil >= 1) {
+      console.log(
+        `There are ${this.daysToSpoil} days left before ${this.name} spoils.`
+      );
+    } else {
+      console.log(`${this.name} has spoiled.`);
+    }
+  }
+
+  aDayPasses() {
+    return this.isFresh(), this.daysToSpoil--;
+  }
+}
+const Banana = new Food("banana", 3, false);
+Banana.isFresh();
+
+class BadFood extends Food {
+  constructor(name, daysToSpoil = 20, edible = true, weapons) {
+    super(name, daysToSpoil);
+    this.edible = edible;
+    this.weapons = weapons || [3, 4, 5];
+  }
+ 
+  isFresh() {
+    if (this.edible && this.daysToSpoil > 1) {
+      console.log(
+        `${this.name} has ${this.daysToSpoil} days left to spoil, you can eat it `
+      );
+    } else if ( this.daysToSpoil === 1) {
+      console.log(
+        `${this.name} has ${this.daysToSpoil} day left to spoil, you cannot eat it `
+      );
+    }
+  
+  }
+ 
+  //    or `I am <name> and you are just a passing trend!`
+  prepare() {
+    
+      const {weapons, name } = this
+      const amunitions = weapons.map(num => num).reduce((a, b ) => a += b, 0)
+      if(amunitions > 20){
+          console.log(`I am ${name} and my calories are too high to count!`);
+
+      }
+  }
+}
+
+const badFood = new BadFood('Pizza', 1, false);
+badFood.isFresh();
+badFood. prepare();
 
 // Do not edit below this line
 module.exports = Food;
--- tamagotchi.js
+++ tamagotchi.js
@@ -1,4 +1,90 @@
 // Create class below
 
+class Tamagotchi {
+  constructor(name, energy, full, mood, sick = false, rehomed = false) {
+    this.name = name;
+    this.energy = energy || 9;
+    this.full = full || 8;
+    this.mood = mood || 6;
+    this.sick = sick;
+    this.rehomed = rehomed;
+  }
+  greet() {
+    console.lo(`Hello, I'm ${this.name}!`);
+  }
+
+  medicate() {
+      if(!this.sick){
+          console.log('refusal to take medicine')
+          this.energy--
+      } else {
+        this.full = 9
+        this.energy -= 3
+        this.sick = !this.sick
+      }
+  }
+
+  status(){}
+
+  eat(){
+    this.full += 2
+    this.energy--
+    if(this.full > 10){
+        this.sick = !this.sick
+    }
+  }
+  //  play: increases mood by 2, reduces energy and full by 1,
+  //  will not play if tamagotchi is sick,
+  //  if asked to play when sick, reduce mood and energy by 1
+  //  will not play if mood is above 9, reduce energy by 2 and full by 1
+  //  will not play if energy is less than or equal to 3
+  //  console log "I am too tired to play" - reduce energy by 1
+
+  play() {
+    if (this.sick) {//if(this.sick === true)
+      this.mood--;
+      this.energy--;
+    } else if (this.mood > 9) {
+      this.full--;
+      this.energy -= 2;
+    } else if(this.energy <= 3) {
+      this.energy--;
+      console.log(`I am too tired to play`);
+    } 
+    else {
+        this.mood += 2;
+       this.energy--;
+       this.full--;
+
+     }
+  }
+
+  sleep(){
+      this.energy += 4
+      this.full -= 3
+  }
+timePasses(){
+if(!this.sick){
+    this.mood -= 2;
+    this.full--;
+    this.energy--;
+} else {
+    this.mood -= 3;
+    this.full -= 2;
+    this.energy -= 2;
+}
+}
+
+badGuardian(){
+    if(this.energy <= 0 || this.full <= 0 && this.mood <= 0){
+        this.rehomed = !this.rehomed
+    }
+}
+
+
+}
+
+const tamagotchi = new Tamagotchi()
+tamagotchi.medicate()
 // Do not edit below this line
 module.exports = Tamagotchi;
