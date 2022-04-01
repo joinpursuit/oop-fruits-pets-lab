1976c777fd09b5ac95d7b8f36a637911368fe9ac
Taylor W <taylorwilliams@pursuit.org>
2022-04-01 17:11:21 -0400

    Some tests completed for Tamagotchi

--- tamagotchi.js
+++ tamagotchi.js
@@ -1,7 +1,40 @@
 // Create class below
 class Tamagotchi {
-  constructor() {}
+  constructor(
+    name,
+    energy = 9,
+    full = 8,
+    mood = 6,
+    sick = false,
+    rehomed = false
+  ) {
+    this.name = name;
+    this.energy = energy;
+    this.full = full;
+    this.mood = mood;
+    this.sick = sick;
+    this.rehomed = rehomed;
+  }
+
+  greet() {
+    console.log(`Hello, I'm ${this.name}`);
+  }
+  status() {
+    console.log(this.energy, this.full, this.mood, this.sick);
+  }
+
+  eat() {
+    this.full += 2;
+    this.energy--;
+    if (this.full > 10) {
+      this.sick = true;
+    }
+  }
 }
 
+const pet = new Tamagotchi("pet", 4, 7, 3, false, true);
+pet.greet();
+pet.status();
+pet.eat();
 // Do not edit below this line
 module.exports = Tamagotchi;
