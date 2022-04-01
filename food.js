87304196c48f840462714d57821a25fde088c05c
Milton Clark <kingcoder247@gmail.com>
2022-04-01 16:36:56 -0400

    All test pass for food.js methods

--- food.js
+++ food.js
@@ -1,4 +1,35 @@
 // Create class below
+class Food{
+    constructor(name, daysToSpoil, fresh = true){
+        this.name = name;
+        this.daysToSpoil = daysToSpoil;
+        this.fresh = fresh;
+    };
+
+    prepare(name){
+        console.log(`${this.name} is being prepared`);
+    };
+    isFresh(name, daysToSpoil){
+        if(this.daysToSpoil >= 1){
+            this.fresh = true;
+            console.log(`There are ${this.daysToSpoil} days left before ${this.name} spoils.`);  
+        } else{
+            this.fresh = false;
+            console.log(`${this.name} has spoiled.`);
+        };
+    };
+    aDayPasses(name){
+        this.daysToSpoil -= 1;
+        return this.isFresh(name, this.daysToSpoil);
+    };
+};
+
+const chicken = new Food("Chicken", 0);
+chicken.prepare(chicken.name);
+chicken.isFresh(chicken.name, chicken.daysToSpoil);
+chicken.aDayPasses(chicken.name, chicken.daysToSpoil);
+
+
 
 // Do not edit below this line
 module.exports = Food;
--- tamagotchi.js
+++ tamagotchi.js
@@ -1,4 +1,49 @@
 // Create class below
+class Tamagotchi{
+    constructor(name, energy = 9, full = 8, mood = 6, sick = false, rehomed = false){
+        this.name = name;
+        this.energy = energy;
+        this.full = full;
+        this.mood = mood;
+        this.sick = sick;
+        this.rehomed = rehomed;
+    };
+    greet(name){
+        return `Hello, I'm ${name}!`;
+    };
+    status(){
+        console.log(`My mood is: ${this.mood}\n I am this full: ${this.full}\n My energy is: ${this.energy}\n I am ${this.sick ? 'sick': 'not sick'}`);
+    };
+    eat(){
+        this.full += 2;
+        this.energy -= 1;
+        if(this.full > 10){
+            return this.sick = true;
+        };
+    };
+    medicate(){
+
+    };
+    play(){
+
+    };
+    sleep(){
+
+    };
+    timePasses(){
+
+    };
+    badGuardian(){
+
+    };
+
+
+
+
+
+
+    
+}
 
 // Do not edit below this line
 module.exports = Tamagotchi;
