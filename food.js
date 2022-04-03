791dcb0fb22a8128c0847fdcd9a107762d32bd87
Christina Loiacono <loiaconochristina@gmail.com>
2022-04-03 10:12:31 -0400

    working on bonus

--- food.js
+++ food.js
@@ -1,5 +1,5 @@
 // Create class below
-class Food{
+class Food {
     constructor(name, daysToSpoil, fresh = true){
         this.name = name;
         this.daysToSpoil = daysToSpoil;
@@ -22,6 +22,36 @@ class Food{
         this.isFresh();
     }
 }
+
+// Bonus: Bad Foods Gone Bad
+class BadFood extends Food {
+    constructor(name, daysToSpoil, fresh, weapons){
+        super(name, daysToSpoil, fresh);
+        this.weapons = weapons;
+    }
+    // inherited
+    isFresh(){}
+    // overwrite this method
+    prepare(){
+        console.log(`I am ${this.name} and my calories are too high to count!`)
+        console.log(`I am ${this.name} and you are just a passing trend!`)
+    }
+    // new method
+    fight(BadFood){
+        console.log(`${this.name} is down ${this.name.daysToSpoil} , but I am still up ${this.name.daysToSpoil} !`)
+    }
+}
+
+// instances of `BadFood`s
+let donut = new BadFood("donut")
+let pizza = new BadFood("pizza")
+console.log(donut.prepare())
+console.log(donut.fight(pizza))
+console.log(pizza.prepare())
+console.log(pizza.fight(donut))
+
+let rottenFood = new BadFood("apple", 20)
+console.log(rottenFood)
 
 // Do not edit below this line
 module.exports = Food;
