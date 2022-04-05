72fa839587a4ccdcf8d90cb801a29d2a5a2b3ca0
vanessaconti <vconti901@aol.com>
2022-04-05 00:37:25 -0400

    added food class and methods

--- food.js
+++ food.js
@@ -1,4 +1,16 @@
 // Create class below
 
+class Food {
+  constructor(name, daysToSpoil, fresh = true) {
+    this.name = name;
+    this.daysToSpoil = daysToSpoil;
+    this.fresh = true;
+  }
+
+  prepare() {
+    console.log(`${this.name} is being prepared`);
+  }
+}
+
 // Do not edit below this line
 module.exports = Food;
