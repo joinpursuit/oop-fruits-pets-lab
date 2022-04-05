09d6bccbe3143bf7c2a304e4f7452aa324e4652b
andres ballares <andr3sb@hotmail.com>
2022-04-05 01:07:18 -0400

    added methods: isFresh and aDayPases

--- food.js
+++ food.js
@@ -1,4 +1,17 @@
 // Create class below
 
+class Food {
+    constructor(){
+        this.name = 'banana';
+        this.daysToSpoil = 0;
+        this.fresh = true; 
+
+    }
+    prepare(){
+        console.log(`food ${this.name} is being prepared');
+    }
+}
+const guanabana = new Food();
+console.log(guanabana.name)
 // Do not edit below this line
 module.exports = Food;
