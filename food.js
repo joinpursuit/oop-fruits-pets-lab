79792b5a0547a279673a2597c2c4c22e49b19f26
andres ballares <andr3sb@hotmail.com>
2022-04-05 01:20:22 -0400

    added tamagotchi class nad greet method

--- food.js
+++ food.js
@@ -1,17 +1,45 @@
 // Create class below
 
 class Food {
-    constructor(){
-        this.name = 'banana';
+    constructor(name, daysToSpoil, fresh){
+        this.name = 'name';
         this.daysToSpoil = 0;
         this.fresh = true; 
 
     }
     prepare(){
-        console.log(`food ${this.name} is being prepared');
+        console.log(`food ${this.name} is being prepared`);
+    }
+
+    isFresh(daysToSpoil){
+        if(daysToSpoil === 0){
+            console.log(`ice cream has spoiled.`)
+        } else {
+            console.log(`There are ${daysToSpoil} days left before ice cream spoils`)
+        }
+    }
+
+    aDayPasses(){
+        if(this.isFresh() > 0){
+            return this.daysToSpoil --;
+        }
     }
 }
-const guanabana = new Food();
-console.log(guanabana.name)
+const guanabana = new Food('banana', 3, true);
+
+console.log(guanabana.prepare())
+console.log(guanabana)
+console.log(guanabana.isFresh(0))
+guanabana.aDayPasses(1)
+
+
+
+
+
+
+
+
+
+
 // Do not edit below this line
 module.exports = Food;
--- tamagotchi.js
+++ tamagotchi.js
@@ -1,4 +1,22 @@
 // Create class below
 
+class tamagotchi {
+    constructor(name, energy, full, mood, sick, rehomed){
+        this.name = 'string';
+        this.energy = 9;
+        this.full = 8;
+        this.mood = 6;
+        this.sick = false;
+        this.rehomed = false;
+    }
+
+    greet(){
+        console.log(`Hello, I'm ${this.name}!`)
+    }
+
+}
+
+const tuki = new tamagotchi(tuki, 100, 7, 6, false, false);
+console.log(tuki)
 // Do not edit below this line
 module.exports = Tamagotchi;
