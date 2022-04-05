9115e5705a40e939276d12329d6d667432aa8959
RonnieGarcia1 <86433181+RonnieGarcia1@users.noreply.github.com>
2022-04-05 02:42:18 -0400

    added logic to methods. test passing for food.js

--- food.js
+++ food.js
@@ -1,21 +1,28 @@
 // Create class below
 class Food {
   constructor(name, daysToSpoil, fresh=true){
-  this.name = name;
-  this.daysToSpoil = daysToSpoil;
-  this.fresh = fresh;
-  }
-  prepare(name){
-    console.log(`${name} is being prepared`)
-  }
+    this.name = name;
+    this.daysToSpoil = daysToSpoil;
+    this.fresh = fresh;
+  };
 
-  isFresh(fresh){
+  isFresh(){
+    if(this.fresh === true && this.daysToSpoil > 0){
+      console.log(`There are ${this.daysToSpoil} days left before Banana spoils.`)
+    } else {
+      console.log(`${this.name} has spoiled.`)
+    };
+  };
 
-  }
+  prepare(){
+    console.log(`${this.name} is being prepared`)
+  };
+
   aDayPasses(){
-    
-  }
-}
+    this.daysToSpoil--;
+    this.isFresh();
+  };
+};
 
 // Do not edit below this line
 module.exports = Food;
