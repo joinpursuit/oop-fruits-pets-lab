a3cb578e64f88319f1cca606f309142f74b53edb
Laiba Sajid Khan <sajid.laiba378@gmail.com>
2022-04-05 18:20:18 -0400

    finished food, now on tamagathci

--- food.js
+++ food.js
@@ -5,9 +5,19 @@ class Food {
         this.fresh = fresh;
     }
     prepare(){
-        
+        console.log(`${this.name} is being prepared`)
     }
-    
+    isFresh(){
+        if(this.fresh === true && this.daysToSpoil > 0){
+            console.log(`There are ${this.daysToSpoil} days left before ${this.name} spoils.`);
+        } else {
+            console.log(`${this.name} has spoiled.`);
+        }
+    }
+    aDayPasses(){
+        this.daysToSpoil = this.daysToSpoil - 1;
+        this.isFresh()
+    }
 }
 
 // Do not edit below this line
