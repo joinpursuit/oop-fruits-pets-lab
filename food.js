c53436bfb205768831efd21d59440dced51d8b61
RonnieGarcia1 <86433181+RonnieGarcia1@users.noreply.github.com>
2022-04-05 02:07:01 -0400

    added food & tamagotchi classes, properties & methods

--- food.js
+++ food.js
@@ -1,4 +1,21 @@
 // Create class below
+class Food {
+  constructor(name, daysToSpoil, fresh=true){
+  this.name = name;
+  this.daysToSpoil = daysToSpoil;
+  this.fresh = fresh;
+  }
+  prepare(name){
+    console.log(`${name} is being prepared`)
+  }
+
+  isFresh(fresh){
+
+  }
+  aDayPasses(){
+    
+  }
+}
 
 // Do not edit below this line
 module.exports = Food;
--- tamagotchi.js
+++ tamagotchi.js
@@ -1,4 +1,23 @@
 // Create class below
+class Tamagotchi {
+  constructor(name, energy=9, full=8, mood=6, sick=false,rehomed=false ){
+      this.name = name;
+      this.energy = energy;
+      this.mood = mood;
+      this.sick = sick;
+      this.full = full;
+      this.rehomed = false
+  }
+  greet(){
 
+  }
+  status(){
+
+  }
+  eat(){
+
+  }
+
+}
 // Do not edit below this line
 module.exports = Tamagotchi;
