0469bd909d2d0b28f1011b83e09ee22f16002369
vanessaconti <vconti901@aol.com>
2022-04-05 14:31:10 -0400

    food tests pass, started tamagotchi class

--- food.js
+++ food.js
@@ -10,7 +10,28 @@ class Food {
   prepare() {
     console.log(`${this.name} is being prepared`);
   }
+
+  isFresh() {
+    if (this.daysToSpoil >= "0") {
+      console.log(
+        `There are ${this.daysToSpoil} days left before ${this.name} spoils.`
+      );
+    } else {
+      console.log(`${this.name} has spoiled.`);
+    }
+  }
+  aDayPasses() {
+    this.daysToSpoil = this.daysToSpoil - 1;
+    this.isFresh();
+  }
 }
+
+const moochi = new Food("moochi", 3);
+console.log(moochi);
+console.log(moochi.name);
+console.log(moochi.prepare());
+console.log(moochi.isFresh());
+console.log(moochi.aDayPasses());
 
 // Do not edit below this line
 module.exports = Food;
--- tamagotchi.js
+++ tamagotchi.js
@@ -1,4 +1,22 @@
 // Create class below
 
+class Tamagotchi {
+  constructor(
+    name,
+    energy = 9,
+    full = 8,
+    mood = 6,
+    sick = false,
+    rehomed = false
+  ) {
+    this.name = name;
+    this.energy = energy;
+    this.full = full;
+    this.mood = mood;
+    this.sick = sick;
+    this.rehomed = rehomed;
+  }
+}
+
 // Do not edit below this line
 module.exports = Tamagotchi;
