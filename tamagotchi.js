3c90b3f38a0becbb22be0830a5e812fff1851ce7
Angie G <angieg@Angies-MBP.fios-router.home>
2022-04-04 23:36:01 -0400

    Add Greet, Status, Eat Methods

--- tamagotchi.js
+++ tamagotchi.js
@@ -1,4 +1,37 @@
 // Create class below
+class Tamagotchi {
+  constructor(
+    name,
+    energy = 9,
+    full = 8,
+    mood = 6,
+    sick = false,
+    rehomed = false
+  ) {
+    this.name = name;
+    this.energy = energy;
+    this.full = full;
+    this.mood = mood;
+    this.sick = sick;
+    this.rehomed = rehomed;
+  }
 
+  greet() {
+    console.log(`Hello, I'm ${this.name}`);
+  }
+
+  status() {
+    console.log(this.energy, this.full, this.mood, this.sick);
+  }
+
+  eat() {
+    this.full;
+  }
+}
+
+const pet = new Tamagotchi("pet", 4, 7, 3, false, true);
+pet.greet();
+pet.status();
+pet.eat();
 // Do not edit below this line
 module.exports = Tamagotchi;
