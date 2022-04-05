0a4c299a591e7b3923980586fa421fa23f7b461f
vanessaconti <vconti901@aol.com>
2022-04-05 14:54:42 -0400

    finished and passed tests

--- tamagotchi.js
+++ tamagotchi.js
@@ -16,6 +16,75 @@ class Tamagotchi {
     this.sick = sick;
     this.rehomed = rehomed;
   }
+
+  greet() {
+    console.log(`Hello, I'm ${this.name}!`);
+  }
+  status() {
+    console.log(
+      `My energy is: ${this.energy}`,
+      `I am this full: ${this.full}`,
+      `My mood is: ${this.mood}`,
+      "I am not sick"
+    );
+  }
+  eat() {
+    this.full = this.full + 2;
+    this.energy = this.energy - 1;
+    if (this.full > 10) {
+      this.sick = true;
+    }
+  }
+  medicate() {
+    if (this.sick === true) {
+      this.full = 9;
+      this.energy -= 3;
+      this.sick = false;
+    } else {
+      console.log("Refuses to take medicine");
+      this.energy -= 1;
+    }
+  }
+  play() {
+    if (this.sick === true) {
+      this.mood -= 1;
+      this.energy -= 1;
+    } else if (this.mood > 9) {
+      this.energy -= 2;
+      this.full -= 1;
+    } else if (this.energy <= 3) {
+      console.log("I am too tired to play");
+      this.energy -= 1;
+    } else {
+      this.mood += 2;
+      this.energy -= 1;
+      this.full -= 1;
+    }
+  }
+
+  sleep() {
+    this.energy += 4;
+    this.full -= 3;
+  }
+
+  timePasses() {
+    if (this.sick === true) {
+      this.mood -= 3;
+      this.full -= 2;
+      this.energy -= 2;
+    } else {
+      this.mood -= 2;
+      this.full--;
+      this.energy--;
+    }
+  }
+
+  badGuardian() {
+    console.log("Has been rehomed");
+    if (this.energy <= 0 || this.mood <= 0 || this.full <= 0) {
+      this.rehomed = true;
+    }
+  }
 }
 
 // Do not edit below this line
