e5bbe1d30c968a2018d09c5155ca581f9b918f78
Rose Durandisse <rosedurandisse@pursuit.org>
2022-04-07 14:47:44 -0400

    Updated food file

--- food.js
+++ food.js
@@ -9,27 +9,29 @@ class Food {
 
   prepare() {
     //if not is returned and the following statement is console logged, undefined also gets printed to the terminal
-    return `${this.name} is being prepared`;
+    console.log(`${this.name} is being prepared`);
   }
   isFresh() {
-    return this.daysToSpoil === 0
-      ? `${this.name} has spoiled`
-      : this.daysToSpoil === 1
-      ? `There are ${this.daysToSpoil} day left before ${this.name} spoils.`
-      : `There are ${this.daysToSpoil} days left before ${this.name} spoils.`;
+    console.log(
+      this.daysToSpoil <= 0
+        ? `${this.name} has spoiled.`
+        : this.daysToSpoil === 1
+        ? `There are ${this.daysToSpoil} day left before ${this.name} spoils.`
+        : `There are ${this.daysToSpoil} days left before ${this.name} spoils.`
+    );
   }
 
   aDayPasses() {
     this.daysToSpoil--;
-    return this.isFresh();
+    console.log(this.isFresh());
   }
 }
 
-const salad = new Food("salad", 2, false);
+// const salad = new Food("salad", 2, false);
 // console.log(salad.prepare());
 // console.log(salad.isFresh());
-console.log(salad.aDayPasses());
-console.log(salad);
+// console.log(salad.aDayPasses());
+// console.log(salad);
 
 // Do not edit below this line
 module.exports = Food;
