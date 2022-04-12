67204910f4e72a5e5cd69726fa2936ee8f411060
Melissa Lopez <melissalopez@pursuit.org>
2022-04-12 10:12:03 -0400

    was stuck on play for a bit

--- tamagotchi.js
+++ tamagotchi.js
@@ -31,29 +31,56 @@ eat(){
 
 medicate(){
     if(this.sick === true){
-        // this.full += 9;
-        // this.energy -= 3;
+        this.full = 9;
+        this.energy -= 3;
+        this.sick = false;
     }else{
         console.log(`${this.name} refuses to take medicine`);
         this.energy--;
     }
 }
 
-play(){
-    this.energy--;
-    this.full--;
-    this.mood += 2;
-    if(this.sick === true){
-        //do not play if sick
-    }
-    else if(this.mood >= 9){
-//if mood is greater than or equal to 9, don't play
-    }
-    else if(this.energy >= 3){
-
-    }//if energy is greater than or equal to 3 don't play
+// play(){
+//     this.energy--;
+//     this.full--;
+//     this.mood += 2;
+//     if(this.sick){
+//      this.mood--
+//      this.energy--   //do not play if sick
+//     }
+//     else if(this.mood > 9){
+//     this.energy -= 2
+//     this.full--
+//     }
+//     if(this.energy <= 3){
+//         this.energy--
+//         console.log('I am too tired to play')
+//     }//if energy is greater than or equal to 3 don't play
+//     // else{
+//     //     this.mood += 2;
+//     //     this.energy--
+//     //     this.full--
+//     // }
     
+// }
+
+play(){
+    if(this.sick){ //if sick
+        this.mood--
+        this.energy--
+    } else if (this.mood > 9){
+        this.energy -= 2
+        this.full--
+    } else if (this.energy <= 3){
+        console.log("I am too tired to play")
+        this.energy -= 1
+    } else {
+        this.mood += 2
+        this.energy--
+        this.full--
+    }
 }
+
 
 sleep(){
     this.energy += 4;
