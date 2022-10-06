600320e0073ed53758fdc3a09ed60c2f5c2d780e
CandaceNia <98069665+CandaceNia@users.noreply.github.com>
2022-10-06 16:45:05 -0700

    Done.

--- tamagotchi.js
+++ tamagotchi.js
@@ -1,4 +1,94 @@
 // Create class below
-
+class Tamagotchi {
+    constructor(
+      name,
+      energy = 9,
+      full = 8,
+      mood = 6,
+      sick = false,
+      rehomed = false
+    ) {
+      this.name = name;
+      this.energy = energy;
+      this.full = full;
+      this.mood = mood;
+      this.sick = sick;
+      this.rehomed = rehomed;
+    }
+    greet() {
+      console.log(`Hello, my name is ${this.name}!`);
+    }
+  
+    status() {
+      return `
+          My mood is: ${this.mood}
+         I am this: ${this.full}
+         My energy is: ${this.energy}
+         ${this.sick ? `I am sick.` : `I am not sick.`}`;
+    }
+  
+    eat() {
+      this.full += 2;
+      this.energy --;
+      if (this.full > 10) {
+        this.sick = true;
+      }
+    }
+  
+    medicate() {
+      if (this.sick) {
+        this.sick = false;
+        this.full = 9;
+        this.energy -= 3;
+      } else {
+        console.log(`Refusal to take medicine`);
+        this.energy --;
+      }
+    }
+  
+    play() {
+      if (this.sick) {
+        this.mood--;
+        this.energy--;
+      } else if (this.mood > 9) {
+        this.energy -= 2;
+        this.full--;
+      } else if (this.energy <= 3) {
+        console.log("I am too tired to play");
+        this.energy--;
+      } else {
+        this.mood += 2;
+        this.energy--;
+        this.full--;
+      }
+    }
+  
+    sleep() {
+      this.energy += 4;
+      this.full -= 3;
+    }
+  
+    timePasses() {
+      if (this.sick) {
+        this.mood -= 3;
+        this.full -= 2;
+        this.energy -= 2;
+      } else {
+        this.mood -= 2;
+        this.full--;
+        this.energy--;
+      }
+    }
+  
+    badGuardian() {;
+      if (this.energy <= 0) {
+        this.rehomed = true;
+      }
+      if (this.full <= 0) {
+        this.rehomed = true;
+        return "Tamagotchi has been removed from your care 😢."
+      }
+    }
+  }
 // Do not edit below this line
 module.exports = Tamagotchi;
