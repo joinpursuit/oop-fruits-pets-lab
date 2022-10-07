a5314cc06683d79ad55a26a197d85757f196eb15
lchen6283 <lchen6283@gmail.com>
2022-10-07 11:35:52 -0400

    food class

--- food.js
+++ food.js
@@ -1,4 +1,24 @@
 // Create class below
 
+class Food {
+  constructor(name) {
+    this.name = name;
+    this.daysToSpoil = 3;
+    this.fresh = true;
+  }
+  prepare() {
+    console.log(`${this.name} is being prepared`);
+  }
+  isFresh() {
+    if (this.daysToSpoil < 0) {
+      console.log(
+        `There are ${this.daysToSpoil} days left before ${this.name} spoils.`
+      );
+    } else {
+      console.log("eeewww what an old banana has spoiled.");
+    }
+  }
+}
+
 // Do not edit below this line
 module.exports = Food;
