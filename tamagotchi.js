1f4fffe1b8cec44b02ecbd8f0797a84ca4de5d72
lchen6283 <lchen6283@gmail.com>
2022-10-07 13:49:38 -0400

    tama class done

--- tamagotchi.js
+++ tamagotchi.js
@@ -16,25 +16,69 @@ class Tamagotchi {
     console.log(`My mood is: ${this.mood} 
       I am this full: ${this.full}
       My energy is: ${this.energy}
+      I am sick.
       `);
-    if (this.sick === false) {
-      console.log("I am not sick");
-    } else {
-      ("I am sick");
-    }
   }
   eat() {
     this.full += 2;
     this.energy -= 1;
     if (this.full > 10) {
-      this.sick === true;
+      this.sick = true;
     }
   }
-  medicate() {}
-  play() {}
-  sleep() {}
-  timePasses() {}
-  badGuardian() {}
+  medicate() {
+    if (this.sick === true) {
+      this.full = 9;
+      this.energy -= 3;
+      this.sick = false;
+    } else {
+      console.log("I refuse to take medicine since I am not sick.");
+      this.energy -= 1;
+    }
+  }
+  play() {
+    if (this.sick === true) {
+      this.mood -= 1;
+      this.energy -= 1;
+    } else if (this.mood > 9) {
+      this.full -= 1;
+      this.energy -= 2;
+    } else if (this.energy <= 3) {
+      console.log("I am too tired to play");
+      this.energy -= 1;
+    } else {
+      this.mood += 2;
+      this.energy -= 1;
+      this.full -= 1;
+    }
+  }
+  sleep() {
+    this.energy += 4;
+    this.full -= 3;
+  }
+  timePasses() {
+    if (this.sick === false) {
+      this.mood -= 2;
+      this.full -= 1;
+      this.energy -= 1;
+    } else {
+      this.mood -= 3;
+      this.full -= 2;
+      this.energy -= 2;
+    }
+  }
+  badGuardian() {
+    console.log("The tamagotchi has been rehomed");
+    if (this.energy <= 0) {
+      this.rehomed = true;
+    }
+    if (this.mood <= 0) {
+      this.rehomed = true;
+    }
+    if (this.full <= 0) {
+      this.rehomed = true;
+    }
+  }
 }
 
 // Do not edit below this line
