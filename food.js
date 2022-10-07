668cd463a2e1bc410b63a3e528d04116a5384d0b
lily08 <liliangelxd@gmail.com>
2022-10-07 16:28:54 -0400

    prepare, isFresh, adaypasses methods

--- food.js
+++ food.js
@@ -2,11 +2,26 @@
 class Food {
   constructor(name, fresh = true) {
     this.name = name
-    this.daysToSpoil = 3
+    this.daysToSpoil = 9
     this.fresh = fresh
   }
-    
-
+  prepare() {
+    return `${this.name} is being prepared`
+  }
+  isFresh() {
+    if (this.daysToSpoil > 0) {
+      return `There are ${this.daysToSpoil} days left before ice cream spoils`
+    } else {
+      return `ice cream has spoiled`
+    }
+  }
+    aDayPasses() {
+      this.daysToSpoil--
+  }
 }
+let fresh = new Food('apple', true)
 // Do not edit below this line
+console.log(fresh.aDayPasses())
+console.log(fresh.isFresh())
+console.log(fresh.prepare())
 module.exports = Food
