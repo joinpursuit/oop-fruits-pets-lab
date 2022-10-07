6b709cb06380d009c4a8f515bd25417e60755d34
arupay <arupay11374@gmail.com>
2022-10-07 11:47:41 -0400

    greet status eat medicate complete

--- tamagotchi.js
+++ tamagotchi.js
@@ -8,6 +8,30 @@ class Tamagotchi {
     this.sick = sick || false;
     this.rehomed = rehomed || false;
   }
+  greet() {
+    `Hello I'm ${this.name}`;
+  }
+  status() {
+    console.log(`My mood is: ${this.mood}
+    I am this full: ${this.full}
+    My energy is: ${this.energy}
+    ${this.sick ? "I am sick" : "I am not sick"}`);
+  }
+  eat() {
+    this.full += 2;
+    this.energy -= 1;
+    this.full > 10 ? (this.sick = true) : null;
+  }
+  medicate() {
+    if (this.sick) {
+      this.sick = false;
+      this.full = 9;
+      this.energy -= 3;
+    } else {
+      console.log("refusal to take medicine");
+      this.energy -= 1;
+    }
+  }
 }
 // Do not edit below this line
 module.exports = Tamagotchi;
