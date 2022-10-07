2e6fe84b0e7179cdcbced0b45c3dbfdf1a3679d6
arupay <arupay11374@gmail.com>
2022-10-07 11:37:21 -0400

    food class created and instance passes testing

--- food.js
+++ food.js
@@ -1,4 +1,27 @@
 // Create class below
+class Food {
+  constructor(name, daysToSpoil, fresh) {
+    this.name = name;
+    this.daysToSpoil = daysToSpoil;
+    this.fresh = fresh || true;
+  }
+  prepare() {
+    console.log(`${this.name} is being prepared`);
+  }
+  isFresh() {
+    this.daysToSpoil > 0
+      ? console.log(
+          `There are ${this.daysToSpoil} days left before ${this.name} spoils.`
+        )
+      : console.log(`${this.name} has spoiled.`);
+  }
+  aDayPasses() {
+    this.daysToSpoil -= 1;
+    return this.isFresh();
+  }
+}
+
+const apple = new Food("apple", 10, true);
 
 // Do not edit below this line
 module.exports = Food;
