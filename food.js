53dd0c826f52d1aba04c2a93caa21c61855a6962
PratimaRoy <pratimaroy170@gmail.com>
2022-10-10 17:01:17 -0400

    Created a tamagatchi class

--- food.js
+++ food.js
@@ -8,5 +8,20 @@ class Food {
      prepare() {
       console.log(`${this.name} is being prepared`);
      }
+     isFresh() {
+        if (this.daysToSpoil > 0) {
+          console.log(
+            `There are ${this.daysToSpoil} days left before ${this.name} spoils.`
+          );
+        } else {
+          console.log(`${this.name} has spoiled.`);
+          this.fresh = false;
+        }
+      }
+      aDayPasses() {
+        --this.daysToSpoil;
+        this.isFresh();
+      }
+    }
 // Do not edit below this line
 module.exports = Food;
--- tamagotchi.js
+++ tamagotchi.js
@@ -1,4 +1,12 @@
 // Create class below
-
+class Tamagotchi {
+    constructor(name) {
+      this.name = name;
+      this.energy = 9;
+      this.full = 8;
+      this.mood = 6;
+      this.sick = false;
+      this.rehomed = false;
+    }
 // Do not edit below this line
 module.exports = Tamagotchi;
