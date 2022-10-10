55954981c8b6b6c1d85dd618c542ee3a14c8f17d
Lortiz528 <lortiztech@gmail.com>
2022-10-10 17:17:20 -0400

    up to medicate passed

--- tamagotchi.js
+++ tamagotchi.js
@@ -1,4 +1,54 @@
 // Create class below
 
+class Tamagotchi {
+  constructor(
+    name,
+    energy = 9,
+    full = 8,
+    mood = 6,
+    sick = false,
+    rehomed = false
+  ) {
+    this.name = name;
+    this.energy = energy;
+    this.full = full;
+    this.mood = mood;
+    this.sick = sick;
+    this.rehomed = rehomed;
+  }
+  greet() {
+    console.log(`Hello, I'm ${this.name}`);
+  }
+
+  status() {
+    console.log(`My moods is: ${this.mood}`);
+    console.log(`I am this full: ${this.full}`);
+    console.log(`My energy is: ${this.energy}`);
+    console.log(`${this.sick ? 'I am sick' : 'I am not sick'}`);
+  }
+
+  eat() {
+    this.full += 2;
+    this.energy -= 1;
+    if (this.full > 10) {
+      this.sick = true;
+    }
+  }
+
+  medicate() {
+    if (this.sick) {
+      this.full = 9;
+      this.energy -= 3;
+      this.sick = false;
+    } else {
+      console.log("refusal to take medicine");
+      this.energy -= 1;
+    }
+  }
+
+
+
+}
+
 // Do not edit below this line
 module.exports = Tamagotchi;
