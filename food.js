ae12c200c305e68d7d0f55c7bb9907974745390e
Kenyetta Griffin <kenyettagriffin@pursuit.org>
2022-10-10 13:27:53 -0400

    "class and methods"

--- food.js
+++ food.js
@@ -1,4 +1,46 @@
 // Create class below
 
+class Food {
+    constructor(name, daysToSpoil, fresh){
+        this.name = name;
+        this.daysToSpoil = daysToSpoil;
+        this.fresh = true;
+    }
+    prepare(){
+        console.log(`${this.name} is being prepared`)
+    }
+    isFresh(){
+        if(this.daysToSpoil > 0) {
+            console.log(`There are ${this.daysToSpoil} days left before the ${this.name} spoils`)
+        }else{
+            console.log(`${this.name} has spoiled`)
+        }
+    }
+    aDayPasses(){
+        this.daysToSpoil
+        return this.isFresh
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 // Do not edit below this line
 module.exports = Food;
