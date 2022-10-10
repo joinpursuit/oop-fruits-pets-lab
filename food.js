acd749cf1143fdca313181665251719c15974a8f
Derek <plutus.derekpeterson@gmail.com>
2022-10-10 16:24:10 -0400

    Completed Food Class, passed all tests

--- food.js
+++ food.js
@@ -1,4 +1,32 @@
-// Create class below
+// Create a food class below:
+
+class Food {
+  constructor(name, daysToSpoil, fresh = true) {
+    this.name = name;
+    this.daysToSpoil = daysToSpoil;
+    this.fresh = fresh;
+  }
+
+  prepare() {
+    console.log(`${this.name} is being prepared`);
+  }
+
+  isFresh() {
+    if (this.daysToSpoil > 0) {
+      this.daysToSpoil = this.daysToSpoil;
+      console.log(
+        `There are ${this.daysToSpoil} days left before ${this.name} spoils.`
+      );
+    } else {
+      console.log(`${this.name} has spoiled.`);
+    }
+  }
+
+  aDayPasses() {
+    this.daysToSpoil -= 1;
+    this.isFresh();
+  }
+}
 
 // Do not edit below this line
 module.exports = Food;
