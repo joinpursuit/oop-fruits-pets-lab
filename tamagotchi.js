84ebc981e098da78d4fb2354661f073b5f055965
Lortiz528 <lortiztech@gmail.com>
2022-10-10 17:32:42 -0400

    passed all

--- tamagotchi.js
+++ tamagotchi.js
@@ -41,13 +41,48 @@ class Tamagotchi {
       this.energy -= 3;
       this.sick = false;
     } else {
-      console.log("refusal to take medicine");
+      console.log('refusal to take medicine');
+      this.energy -= 1;
+    }
+  }
+  play() {
+    if (this.sick) {
+      this.mood -= 1;
+      this.energy -= 1;
+    } else if (this.mood > 9) {
+      this.energy -= 2;
+      this.full -= 1;
+    } else if (this.energy <= 3) {
+      console.log('I am too tired to play');
+      this.energy -= 1;
+    } else {
+      this.mood += 2;
+      this.energy -= 1;
+      this.full -= 1;
+    }
+  }
+  sleep() {
+    this.energy += 4;
+    this.full -= 3;
+  }
+  timePasses() {
+    if (this.sick) {
+      this.mood -= 3;
+      this.full -= 2;
+      this.energy -= 2;
+    } else {
+      this.mood -= 2;
+      this.full -= 1;
       this.energy -= 1;
     }
   }
 
-
-
+  badGuardian() {
+    if (this.energy <= 0 || this.mood <= 0 || this.full <= 0) {
+      this.rehomed = true;
+      console.log('the tamagotchi has been rehomed');
+    }
+  }
 }
 
 // Do not edit below this line
