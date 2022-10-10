affad17cc31b7ccb01e78947f9c3e46c7a64d6b9
PratimaRoy <pratimaroy170@gmail.com>
2022-10-10 17:00:11 -0400

    Created food class

--- food.js
+++ food.js
@@ -1,4 +1,12 @@
 // Create class below
-
+class Food {
+    constructor(name, daysToSpoil) {
+      this.name = name;
+      this.daysToSpoil = daysToSpoil;
+      this.fresh = true;
+     }
+     prepare() {
+      console.log(`${this.name} is being prepared`);
+     }
 // Do not edit below this line
 module.exports = Food;
