8845499554572c09311add4679a91f66c9c63a18
Kenyetta Griffin <kenyettagriffin@pursuit.org>
2022-10-11 11:59:30 -0400

    Methods

--- tamagotchi.js
+++ tamagotchi.js
@@ -12,6 +12,9 @@ class Tamagotchi{
     greet(){
         console.log(`Hello, I'm ${this.name}`)
     }
+    status(){
+        console.log(`My mood is ${this.mood}, I am this full ${this.full}, my energy is ${this.energy}, I am ${this.sick}`)
+    }
 }
 
 
