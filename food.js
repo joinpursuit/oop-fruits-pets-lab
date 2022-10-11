214dce92b14cb7ecc5e4c3bbaedf3178c3ced750
Khaleed Uddin <uddin1786@gmail.com>
2022-10-11 19:20:18 -0400

    food.js with samantha

--- food.js
+++ food.js
@@ -1,4 +1,32 @@
 // Create class below
+class Food {
+    constructor(name, daysToSpoil, fresh){
+        this.name = name;
+        this.daysToSpoil = daysToSpoil;
+        this.fresh = fresh || true;
+    }
+    prepare(name){
+        console.log(`${name} is being prepared.`)
+    }
+
+    isFresh(name, daysToSpoil){
+        if(daysToSpoil > 0){
+            console.log(`There are ${daysToSpoil} days left before ${name} spoils.`)
+        } else {
+            console.log(`${name} has spoiled`)
+        }
+    }
+
+    aDayPasses(daysToSpoil){
+        daysToSpoil = daysToSpoil - 1;
+       console.log(daysToSpoil);
+    }
+}
+
+const food = new Food();
+console.log(food.aDayPasses(5))
+
+
 
 // Do not edit below this line
 module.exports = Food;
