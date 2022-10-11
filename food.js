811aa8b63a53cd09b0bac838372c26fe903bbf90
Edwin Codrington <edwincodrington@gmail.com>
2022-10-10 23:38:44 -0400

    Food class implemented

--- food.js
+++ food.js
@@ -1,4 +1,32 @@
 // Create class below
+class Food {
+	constructor(name, daysToSpoil, fresh = true) {
+		this.name = name;
+		this.daysToSpoil = daysToSpoil;
+		this.fresh = fresh;
+	}
+	prepare() {
+		console.log(`${this.name} is being prepared`);
+	}
+	isFresh() {
+		console.log(
+			this.daysToSpoil > 0
+				? `There are ${this.daysToSpoil} days left before ${this.name} spoils.`
+				: `${this.name} has spoiled.`
+		);
+	}
+	aDayPasses() {
+		this.daysToSpoil--;
+		if (this.daysToSpoil <= 0) {
+			this.fresh = false;
+		}
+		this.isFresh();
+	}
+}
 
+const iceCream = new Food("ice cream", 6, true);
+iceCream.prepare();
+iceCream.isFresh();
+iceCream.aDayPasses();
 // Do not edit below this line
 module.exports = Food;
