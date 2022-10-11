9149a5693a4dac4efedc4bb1b9e088f26d1d7990
Tom LaTulipe <tlatulipe@gmail.com>
2022-10-10 21:22:58 -0400

    Food.js basics done - Tamogotchi up to eat

--- food.js
+++ food.js
@@ -1,4 +1,34 @@
 // Create class below
+class Food {
+  constructor(name, daysToSpoil) {
+    this.name = name;
+    this.daysToSpoil = daysToSpoil;
+    this.fresh = this.daysToSpoil >= 1 ? true : false;
+  }
 
+  prepare() {
+    console.log(`${this.name} is being prepared`);
+  }
+  isFresh() {
+    this.fresh
+      ? console.log(
+          `There are ${this.daysToSpoil} days left before ${this.name} spoils`
+        )
+      : console.log(`${this.name} has spoiled`);
+  }
+  aDayPasses() {
+    this.daysToSpoil -= 1;
+    this.daysToSpoil <= 0 ? (this.fresh = false) : null;
+    this.isFresh();
+  }
+}
+
+let hamburger = new Food("Hamburger", 3, true);
+hamburger.prepare();
+hamburger.isFresh();
+hamburger.aDayPasses();
+hamburger.aDayPasses();
+hamburger.aDayPasses();
+hamburger.aDayPasses();
 // Do not edit below this line
 module.exports = Food;
--- tamagotchi.js
+++ tamagotchi.js
@@ -1,4 +1,33 @@
 // Create class below
+class Tamogotchi {
+  constructor(name) {
+    this.name = name;
+    this.energy = 9;
+    this.full = 8;
+    this.mood = 6;
+    this.sick = false;
+    this.rehomed = false;
+  }
+
+  greet() {
+    console.log(`Hello, I'm ${this.name}`);
+  }
+  status() {
+    console.log(`****************`);
+    console.log(`My mood is : ${this.mood}`);
+    console.log(`I am this full: ${this.full}`);
+    console.log(`My energy is: ${this.energy}`);
+    this.sick
+      ? console.log(`I am not sick`)
+      : console.log(`I don't feel too good...`);
+  }
+  eat() {
+    this.fullness += 2;
+    this.energy--;
+    this.fullness > 10 ? (this.sick = true) : null;
+  }
+  
+}
 
 // Do not edit below this line
 module.exports = Tamagotchi;
