1fe1290b481b7fbfc4473dcbe7a50dd27fdad24a
Khaleed Uddin <uddin1786@gmail.com>
2022-10-11 19:52:48 -0400

    tamagachi add medicate sleep timePassed methods w/ Samantha

--- tamagotchi.js
+++ tamagotchi.js
@@ -1,6 +1,6 @@
 // Create class below
 class Tamagotchi {
-    constructor(name, energy, full, mood, sick, rehomed){
+    constructor(name, energy, full, mood, sick, rehomed) {
         this.name = name;
         this.energy = 9;
         this.full = 8;
@@ -9,11 +9,11 @@ class Tamagotchi {
         this.rehomed = false;
     }
 
-    greet(){
+    greet() {
         console.log(`Hello, I'm ${this.name}`)
     }
 
-    status(){
+    status() {
         console.log(`My mood is: ${this.mood}
         I am this full: ${this.full}
         My energy is: ${this.energy}
@@ -21,11 +21,59 @@ class Tamagotchi {
         `)
     }
 
-    eat(){
-        
+    eat() {
+        if (this.full >= 10) {
+            this.sick = true;
+            // this.status();
+        }
+        this.full += 2;
+        this.energy--;
     }
 
-    
+    medicate() {
+        if (this.sick) {
+            this.full = 9;
+            this.energy -= 3;
+            this.sick = false;
+        } else {
+            console.log(`Refusal to take medicine`)
+            this.energy--;
+        }
+    }
+
+    play() {
+        if (this.sick) {
+            this.mood--;
+            this.energy--;
+        } else if (this.mood > 9) {
+            this.energy -= 2;
+            this.full--;
+        } else if (this.energy <= 3) {
+            console.log(`I am too tired to play`)
+            this.energy--;
+        } else {
+            this.mood += 2;
+            this.energy--;
+            this.full--;
+        }
+    }
+
+    sleep(){
+        this.energy += 4;
+        this.full -= 3;
+    }
+
+    timePasses(){
+        if (!this.sick){
+            this.mood -= 2;
+            this.full --;
+            this.energy--;
+        } else {
+            this.mood -= 3;
+            this.full -= 2;
+            this.energy -= 2;
+        }
+    }
 }
 // Do not edit below this line
 module.exports = Tamagotchi;
