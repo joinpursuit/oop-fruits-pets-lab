400645decacb4ab15ab4492f25b27102e7b02b6a
Jimmy Ayivor <jimmyayivor@gmail.com>
2022-10-11 22:23:59 -0400

    Solution OOP Lab

--- food.js
+++ food.js
@@ -1,4 +1,26 @@
 // Create class below
+class Food {
+  constructor(name, daysToSpoil, fresh) {
+    this.name = name;
+    this.daysToSpoil = daysToSpoil;
+    this.fresh = fresh || true;
+  }
+  prepare() {
+    console.log(`${this.name} is being prepared`);
+  }
+  isFresh() {
+    if (this.daysToSpoil <= 0) {
+      console.log(`${this.name} has spoiled.`);
+    }
+    console.log(
+      `There are ${this.daysToSpoil} days left before ${this.name} spoils.`
+    );
+  }
+  aDayPasses() {
+    this.isFresh();
+    this.daysToSpoil--;
+  }
+}
 
 // Do not edit below this line
 module.exports = Food;
--- tamagotchi.js
+++ tamagotchi.js
@@ -1,4 +1,71 @@
 // Create class below
+class Tamagotchi {
+  constructor(
+    name,
+    energy = 9,
+    full = 8,
+    mood = 6,
+    sick = false,
+    rehomed = false
+  ) {
+    this.name = name;
+    this.energy = energy;
+    this.full = full;
+    this.mood = mood;
+    this.sick = sick;
+    this.rehomed = rehomed;
+  }
+  greet() {
+    console.log(`Hello, I'm ${this.name}`);
+  }
+  status() {
+    console.log(`energy: ${this.energy} fullness: ${this.full} mood: ${this.mood} is sick: ${this.sick}`);
+  }
+  eat() {
+    this.full += 2;
+    this.energy--;
+    if (this.full > 10) {
+      return (this.sick = true);
+    }
+  }
+  medicate() {
+    this.full = 9;
+    this.sick
+      ? (this.energy -= 3)
+      : this.energy-- && console.log(`refusal to take medicine`);
+    return (this.sick = false);
+  }
+  play() {
+    if (this.sick) {
+      this.mood--;
+      this.energy--;
+    } else if (this.mood > 9) {
+      this.energy -= 2;
+      this.full--;
+    } else if (this.energy <= 3) {
+      console.log("I am too tired to play");
+      this.energy--;
+    } else {
+      this.mood += 2;
+      this.energy--;
+      this.full--;
+    }
+  }
+  sleep() {
+    this.energy += 4;
+    this.full -= 3;
+  }
+  timePasses() {
+    this.sick ? (this.mood -= 3) : (this.mood -= 2);
+    this.sick ? (this.full -= 2) : this.full--;
+    this.sick ? (this.energy -= 2) : this.energy--;
+  }
+  badGuardian() {
+    if (this.full <= 0 || this.energy <= 0 || this.mood <= 0) {
+      return (this.rehomed = true);
+    }
+  }
+}
 
 // Do not edit below this line
 module.exports = Tamagotchi;
