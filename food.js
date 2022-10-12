24d693dd0f8e8b967062117dcbb69f5b112e8d62
Jonathan Scheiber <jon@scheiber.me>
2022-10-12 13:48:51 -0400

    create prepare method

--- food.js
+++ food.js
@@ -1,4 +1,13 @@
-// Create class below
+class Food {
+  constructor(name, daysToSpoil, fresh = true) {
+    this.name = name;
+    this.daysToSpoil = daysToSpoil;
+    this.fresh = fresh;
+  }
 
-// Do not edit below this line
+  prepare() {
+    console.log(`${this.name} is being prepared`);
+  }
+}
+
 module.exports = Food;
