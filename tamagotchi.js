e9201f97e240c30fa50338b39e0c33b84a65fdeb
EmaleeSoto <emaleesoto@gmail.com>
2022-10-12 14:17:13 -0400

    Added greet method to tamagotchi class

--- tamagotchi.js
+++ tamagotchi.js
@@ -1,5 +1,10 @@
 // Create class below
 
+// greet: Tamagotchi console logs Hello, I'm <name>!
+// status: Tamogotchi console logs energy, full, mood, and sick values (see below for sample message)
+// eat: increases fullness by two, decreases energy by 1,
+// if eat method makes fullness more than 10, tamagotchi becomes sick (sick = true)
+
 class Tamagotchi {
   constructor(
     name,
@@ -16,7 +21,14 @@ class Tamagotchi {
     this.sick = sick;
     this.rehomed = rehomed;
   }
+
+  greet() {
+    console.log(`Hello, I'm ${this.name}!`);
+  }
 }
+
+const myPet = new Tamagotchi("Mochi");
+myPet.greet();
 
 // Do not edit below this line
 module.exports = Tamagotchi;
