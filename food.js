41b02272a71f5268a7ee1bda3ca88bfca3843ce9
Jamal Ruiz <jamalruiz@jamals-air-2.mynetworksettings.com>
2022-10-11 21:34:49 -0400

    Lab Complete

--- food.js
+++ food.js
@@ -1,4 +1,23 @@
 // Create class below
+class Food{
+constructor(name,daysToSpoil, fresh = true){
+this.name= name
+this.daysToSpoil= daysToSpoil
+this.fresh=fresh
+}
+prepare(){
+    console.log(`${this.name} is being prepared`);
+}
+isFresh(){
+    if(this.daysToSpoil<=0) {
+        console.log(`${this.name} has spoiled.`)    
+    }
+    console.log(`There are ${this.daysToSpoil} days left before ${this.name} spoils.`)
+}
+aDayPasses(){
+    this.isFresh() - this.daysToSpoil -- 
+}
+}
 
 // Do not edit below this line
 module.exports = Food;
