fabb326a66445cd3c235cb28e24bcd45277f8fdf
Edwin Codrington <edwincodrington@gmail.com>
2022-10-13 02:04:56 -0400

    tamagotchi class implemented

--- tamagotchi.js
+++ tamagotchi.js
@@ -1,4 +1,75 @@
 // Create class below
-
+class Tamagotchi {
+	constructor(name, energy = 9, full = 8, mood = 6, sick = false, rehomed = false) {
+		this.name = name;
+		this.energy = energy;
+		this.full = full;
+		this.mood = mood;
+		this.sick = sick;
+		this.rehomed = rehomed;
+	}
+	greet() {
+		console.log(`Hello, I'm ${this.name}!`);
+	}
+	status() {
+		console.log(
+			`My energy is: ${this.energy}\n I am this full: ${this.full}\n My mood is: ${this.mood}\n Sick:${this.sick}`
+		);
+	}
+	eat() {
+		this.full += 2;
+		this.energy -= 1;
+		if (this.full > 10) {
+			this.sick = true;
+		}
+	}
+	medicate() {
+		if (this.sick) {
+			this.full = 9;
+			this.energy -= 3;
+			this.sick = false;
+		} else {
+			console.log(`${this.name} refused to take medicine!`);
+			this.energy--;
+		}
+	}
+	play() {
+		if (this.sick) {
+			this.mood--;
+			this.energy--;
+		} else if (this.mood > 9) {
+			this.energy -= 2;
+			this.full--;
+		} else if (this.energy <= 3) {
+			console.log(`I am too tired to play`);
+			this.energy--;
+		} else {
+			this.mood += 2;
+			this.energy--;
+			this.full--;
+		}
+	}
+	sleep() {
+		this.energy += 4;
+		this.full -= 3;
+	}
+	timePasses() {
+		if (!this.sick) {
+			this.mood -= 2;
+			this.full--;
+			this.energy--;
+		} else {
+			this.mood -= 3;
+			this.full -= 2;
+			this.energy -= 2;
+		}
+	}
+	badGuardian() {
+		console.log(`${this.name} has been rehomed.`);
+		if (this.energy <= 0 || this.mood <= 0 || this.full <= 0) {
+			this.rehomed = true;
+		}
+	}
+}
 // Do not edit below this line
 module.exports = Tamagotchi;
