9072de0649f55d049ed3b63c066bfc353ea1e041
H.C.Guerrero <clydeguerrero1@gmail.com>
2022-10-12 22:38:32 -0400

    Tamagotchi some methods implemented

--- food.js
+++ food.js
@@ -9,7 +9,7 @@ class Food {
     console.log(`${this.name} is being prepared`);
   }
   isFresh() {
-    if (this.daysToSpoil >= 1 && this.fresh === true) {
+    if (this.daysToSpoil > 0 && this.fresh === true) {
       console.log(
         `There are ${this.daysToSpoil} days left before ${this.name} spoils`,
       );
@@ -31,11 +31,41 @@ Bagel.prepare();
 Bagel.isFresh();
 Bagel.aDayPasses();
 Bagel.isFresh();
-/*
 Apple.prepare();
 Apple.isFresh();
 Cake.prepare();
+Cake.aDayPasses();
 Cake.isFresh();
-*/
+
+class Tamagotchi {
+  constructor(name, energy, full, mood, sick, rehomed) {
+    this.name = name;
+    this.energy = energy;
+    this.full = full;
+    this.mood = mood;
+    this.sick = false;
+    this.rehomed = false;
+  }
+  greet() {
+    console.log(`Hello, I'm ${this.name}!`);
+  }
+  sickState() {
+    if (!this.sick) {
+      this.sick === true;
+      console.log(`I am sick`);
+    } else {
+      console.log(`I am not sick`);
+    }
+  }
+  status() {
+    console.log(
+      `***********\n My mood is: ${this.mood} \n I am this full: ${this.full} 
+    \n My energy is: ${this.energy} \n ` +
+        this.sickState() +
+        `\n ***********`,
+    );
+  }
+}
+
 // Do not edit below this line
 module.exports = Food;
