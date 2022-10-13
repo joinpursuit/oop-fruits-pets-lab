6a8985faf1c9087d3de1141655f79be0d3043412
H.C.Guerrero <clydeguerrero1@gmail.com>
2022-10-12 23:26:38 -0400

    Tamagotchi skeleton implemented

--- food.js
+++ food.js
@@ -20,7 +20,7 @@ class Food {
     }
   }
   aDayPasses() {
-    return (this.daysToSpoil -= 1);
+    this.daysToSpoil -= 1;
   }
 }
 const Bagel = new Food('OnionBagel', 4, true);
@@ -43,8 +43,8 @@ class Tamagotchi {
     this.energy = energy;
     this.full = full;
     this.mood = mood;
-    this.sick = false;
-    this.rehomed = false;
+    this.sick = sick;
+    this.rehomed = rehomed;
   }
   greet() {
     console.log(`Hello, I'm ${this.name}!`);
@@ -65,7 +65,65 @@ class Tamagotchi {
         `\n ***********`,
     );
   }
+  eat() {
+    this.full += 2;
+    this.energy -= 1;
+    if (this.full > 10) {
+      this.sick === true;
+    }
+  }
+  medicate() {
+    if (this.sick === true) {
+      this.full === 9;
+      this.energy -= 3;
+    } else if (this.sick === false) {
+      this.energy -= 1;
+      console.log(`Tomogatchi is healthy, no need to medicate!`);
+    }
+  }
+  play() {
+    if (this.sick === true) {
+      this.mood -= 1;
+      this.energy -= 1;
+    } else if (this.moood > 9) {
+      this.energy -= 2;
+      this.full -= 1;
+    } else if (this.energy <= 3) {
+      console.log('I am too tired to play');
+      this.energy -= 1;
+    }
+  }
+  sleep() {
+    this.energy += 4;
+    this.full -= 3;
+  }
+  timePasses() {
+    if (this.sick === false) {
+      this.mood -= 2;
+      this.full -= 1;
+      this.energy -= 1;
+    } else if (this.sick === true) {
+      this.mood -= 3;
+      this.full -= 2;
+      this.energy -= 2;
+    }
+  }
+  badGuardian() {
+    console.log(
+      `Protective services has seized your Tamagatchi and has been rehomed.`,
+    );
+    if (this.energy <= 0) {
+      this.rehomed === true;
+    } else if (this.mood <= 0) {
+      this.rehomed === true;
+    } else if (this.full <= 0) {
+      this.rehomed === true;
+    }
+  }
 }
 
+const Begley = new Tamagotchi('Begley', 8, 8, 8, false, false);
+const Joffrey = new Tamagotchi('Joffrey', 0, 0, 0, false, false);
+const Scarlet = new Tamagotchi('Scarlet', 5, 5, 5, true, false);
 // Do not edit below this line
 module.exports = Food;
